d637a9893f6c6a38e256e9c7b8b382fb98b3078e
Jeff Forte <jeff.l.forte@gmail.com>
2013-04-27 19:36:26 -0500

    Adding webservice handling

--- src/main/java/com/gnw/credit_omni/soap/actions/WebServicesAction.java
+++ src/main/java/com/gnw/credit_omni/soap/actions/WebServicesAction.java
@@ -3,6 +3,11 @@ package com.gnw.credit_omni.soap.actions;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.log4j.Logger;
 import org.jboss.soa.esb.actions.AbstractActionLifecycle;
 import org.jboss.soa.esb.actions.ActionProcessingException;
@@ -15,6 +20,7 @@ import com.gnw.credit_omni.soap.utils.SOAPUtils;
 public class WebServicesAction extends AbstractActionLifecycle
 {
 
+	private static final String TEST_PUBLIC_WEBSITE_URL = "http://www.equifax.com/home/en_us";
 	protected ConfigTree config;
 	private static final String SCORE_TYPE_ATTR_NAME = "scoreType";
 	
@@ -37,13 +43,56 @@ public class WebServicesAction extends AbstractActionLifecycle
 				  " header: "  + message.getHeader() + 
 				  " context: " + message.getContext());
 		
+		String response = getWebsiteURL(TEST_PUBLIC_WEBSITE_URL);
+		log.info("WebServicesAction.process: response from website  " + WebServicesAction.TEST_PUBLIC_WEBSITE_URL + 
+				  ": " + response);
 		
 		log.info("WebServicesAction.process: return: scoreType=" + SOAPUtils.getValueOfAttributeInMessage(message, "scoreType") +
 				 " messageId="     + SOAPUtils.getValueOfAttributeInMessage(message, "messageID") +
 				 " processedFlag=" + SOAPUtils.getValueOfAttributeInMessage(message, "processedFlag"));
 		
 		return message;
-	}	
+	}
+	
+	/**
+	 * @param url
+	 * @return
+	 */
+	public String getWebsiteURL(String url) {
+		String response = "<no response>";
+		
+	    // Create an instance of HttpClient.
+	    HttpClient client = new HttpClient();
+	    
+		HttpMethod method = new GetMethod(WebServicesAction.TEST_PUBLIC_WEBSITE_URL);
+		   try {
+			   
+			      // Execute the method.
+			      int statusCode = client.executeMethod(method);
+
+			      if (statusCode != HttpStatus.SC_OK) {
+			    	  response = "WebServicesAction.getWebsiteURL: method failed: " + method.getStatusLine();
+			      }
+
+			      // Read the response body.
+			      byte[] responseBody = method.getResponseBody();
+
+			      // Deal with the response.
+			      // Use caution: ensure correct character encoding and is not binary data
+			      response = new String(responseBody);
+
+			    } catch (HttpException e) {
+			      response = "WebServicesAction.getWebsiteURL: fatal protocol violation: " + e.getMessage();
+			    } catch (IOException e) {
+			    	response = "WebServicesAction.getWebsiteURL: fatal transport error: " + e.getMessage();
+			    } finally {
+			      // Release the connection.
+			      method.releaseConnection();
+			    }  
+
+		return response;
+	}
+
 
 	/**
 	 * default success handler
